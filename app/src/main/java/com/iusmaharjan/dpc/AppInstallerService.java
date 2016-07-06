1a5ab740da2e2d12c18f8a92fac6ff5f1db80a26
Ayush Maharjan <ius.maharjan@gmail.com>
2016-07-06 10:37:27 +0545

    download and installation task assigned to service

--- app/src/main/java/com/iusmaharjan/dpc/AppInstallerService.java
+++ app/src/main/java/com/iusmaharjan/dpc/AppInstallerService.java
@@ -1,16 +1,201 @@
 package com.iusmaharjan.dpc;
 
+import android.app.DownloadManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.pm.PackageInstaller;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import timber.log.Timber;
+
 public class AppInstallerService extends Service {
+
+    List<Application> downloadQueue;
+
+    Application currentDownloading;
+
+    DownloadManager downloadManager;
+    long downloadId;
+
+    private IBinder serviceBinder = new ServiceBinder();
+
     public AppInstallerService() {
+    }
+
+    public static final Intent getAppInstallerServiceLaunchIntent(Context context) {
+        return new Intent(context, AppInstallerService.class);
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Queue to hold applications to be downloaded
+        downloadQueue = new LinkedList<>();
+
+        // Get Download Service
+        downloadManager = (DownloadManager)this.getSystemService(Context.DOWNLOAD_SERVICE);
+
+        registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return serviceBinder;
     }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    public void addToDownloadQueue(Application application) {
+        downloadQueue.add(application);
+        downloadNextApp();
+    }
+
+    public void addToDownloadQueueImmediately(Application application) {
+        downloadQueue.add(0, application);
+        downloadNextApp();
+    }
+
+    private void downloadNextApp() {
+        if(currentDownloading == null && !downloadQueue.isEmpty()) {
+            currentDownloading = downloadQueue.remove(0);
+            // Start Current Download
+            download();
+        }
+    }
+
+    private void download() {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(currentDownloading.getDownloadURL()));
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "app.apk");
+        request.setTitle("Downloading "+currentDownloading.getApplicationName()+"...");
+        downloadId = downloadManager.enqueue(request);
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                long downloadId = intent.getLongExtra(
+                        DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                DownloadManager.Query query = new DownloadManager.Query();
+                query.setFilterById(downloadId);
+                Cursor c = downloadManager.query(query);
+                if (c.moveToFirst()) {
+                    int columnIndex = c
+                            .getColumnIndex(DownloadManager.COLUMN_STATUS);
+                    String uriString = c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                    Uri uri = Uri.parse(uriString);
+                    String path = uri.getPath();
+                    if (DownloadManager.STATUS_SUCCESSFUL == c
+                            .getInt(columnIndex)) {
+                        Timber.d(path);
+                        try {
+                            installSilently(new File(path));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Timber.d("Download Successful");
+                    }
+                }
+            }
+        }
+    };
+
+    private void installSilently(final File file) throws IOException {
+
+        // Get package installer
+        PackageInstaller packageInstaller = this.getPackageManager().getPackageInstaller();
+
+        // Define installer params
+        PackageInstaller.SessionParams params =
+                new PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL);
+
+        // Create a session ID
+        int sessionID = packageInstaller.createSession(params);
+
+        // Open Session
+        PackageInstaller.Session session = packageInstaller.openSession(sessionID);
+
+        // Calculate file size
+        long sizeBytes = 0;
+        if (file.isFile())
+        {
+            sizeBytes = file.length();
+        }
+
+        // Open file input stream
+        InputStream in = new FileInputStream(file);
+
+        // Define session as output stream
+        OutputStream out = session.openWrite("installation_session", 0, sizeBytes);
+
+        byte[] buffer = new byte[65536];
+        int c;
+        while ((c = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, c);
+        }
+
+        session.fsync(out);
+
+        in.close();
+        out.close();
+
+        session.commit(createIntentSender(this, sessionID));
+
+        session.close();
+
+        currentDownloading = null;
+        downloadNextApp();
+
+    }
+
+    // TODO Replace with custom intent
+    private static IntentSender createIntentSender(Context context, int sessionId) {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context,
+                sessionId,
+                new Intent(Intent.ACTION_INSTALL_PACKAGE),
+                0);
+        return pendingIntent.getIntentSender();
+    }
+
+    /**
+     * Installs APK with prompt
+     */
+    private void installAPK(File file) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public class ServiceBinder extends Binder {
+        public AppInstallerService getService() {
+            return AppInstallerService.this;
+        }
+    }
+
 }
--- app/src/main/java/com/iusmaharjan/dpc/dpc/DPCPreferenceFragment.java
+++ app/src/main/java/com/iusmaharjan/dpc/dpc/DPCPreferenceFragment.java
@@ -2,39 +2,24 @@ package com.iusmaharjan.dpc.dpc;
 
 
 import android.app.Activity;
-import android.app.DownloadManager;
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.pm.PackageInstaller;
-import android.database.Cursor;
+import android.content.ServiceConnection;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.IBinder;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
-import android.util.Log;
-import android.widget.Toast;
 
+import com.iusmaharjan.dpc.AppInstallerService;
 import com.iusmaharjan.dpc.Application;
 import com.iusmaharjan.dpc.EnterpriseApplicationManager;
 import com.iusmaharjan.dpc.R;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import timber.log.Timber;
 
@@ -44,13 +29,29 @@ public class DPCPreferenceFragment extends PreferenceFragment implements
 
     private static final int SET_DEVICE_ADMIN_REQUEST = 1001;
 
-    private static final String URL_LF_MEETING_ROOM_APP = "https://www.dropbox.com/s/7506st3oi2vjoiq/TestDPC_3008.apk?dl=1";
-
     SwitchPreference prefDeviceAdmin;
     SwitchPreference prefDeviceOwner;
     Preference prefLFMeetingRoomApp;
 
     DPCInterface.Presenter dpcPresenter;
+
+    AppInstallerService boundService;
+    boolean mServiceBound;
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            AppInstallerService.ServiceBinder serviceBinder =
+                    (AppInstallerService.ServiceBinder)iBinder;
+            boundService = serviceBinder.getService();
+            mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mServiceBound = false;
+        }
+    };
 
     public DPCPreferenceFragment() {
 
@@ -78,15 +79,19 @@ public class DPCPreferenceFragment extends PreferenceFragment implements
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        getActivity().registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    public void onStart() {
+        super.onStart();
+        Intent intent = AppInstallerService.getAppInstallerServiceLaunchIntent(getActivity());
+        getActivity().startService(intent);
+        getActivity().bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(receiver);
+    public void onStop() {
+        super.onStop();
+        if(mServiceBound) {
+            getActivity().unbindService(mServiceConnection);
+        }
     }
 
     @Override
@@ -124,8 +129,10 @@ public class DPCPreferenceFragment extends PreferenceFragment implements
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
-
-            download();
+            EnterpriseApplicationManager applicationManager = EnterpriseApplicationManager.getInstance();
+            for(Application application: applicationManager.getNotInstalledApps()) {
+                boundService.addToDownloadQueue(application);
+            }
         }
         return false;
     }
@@ -160,195 +167,5 @@ public class DPCPreferenceFragment extends PreferenceFragment implements
         Timber.d("requestToSetAdmin");
         startActivityForResult(intent, SET_DEVICE_ADMIN_REQUEST);
     }
-
-    public class InstallAPK extends AsyncTask<String,Void,Void> {
-
-        ProgressDialog progressDialog;
-        int status = 0;
-
-        private Context context;
-        public void setContext(Context context, ProgressDialog progress){
-            this.context = context;
-            this.progressDialog = progress;
-        }
-
-        public void onPreExecute() {
-            progressDialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(String... arg0) {
-            try {
-                URL url = new URL(arg0[0]);
-                HttpURLConnection c = (HttpURLConnection) url.openConnection();
-                c.setRequestMethod("GET");
-                c.setDoOutput(false);
-                c.connect();
-
-//                Timber.d("Connection:"+c.toString());
-
-                File sdcard = Environment.getExternalStorageDirectory();
-                File outputFile = new File(sdcard, "app1.apk");
-                if(outputFile.exists()){
-                    outputFile.delete();
-                }
-                FileOutputStream fos = new FileOutputStream(outputFile);
-
-                InputStream is = c.getInputStream();
-
-                byte[] buffer = new byte[1024];
-                int len1 = 0;
-                while ((len1 = is.read(buffer)) != -1) {
-                    fos.write(buffer, 0, len1);
-                }
-                fos.flush();
-                fos.close();
-                is.close();
-
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/app.apk")), "application/vnd.android.package-archive");
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-
-            } catch (FileNotFoundException fnfe) {
-                status = 1;
-                Log.e("File", "FileNotFoundException! " + fnfe);
-            }
-
-            catch(Exception e)
-            {
-                Log.e("UpdateAPP", "Exception " + e);
-            }
-            return null;
-        }
-
-        public void onPostExecute(Void unused) {
-            progressDialog.dismiss();
-            if(status == 1)
-                Toast.makeText(context,"Game Not Available", Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-    private void installAPK() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/app.apk")), "application/vnd.android.package-archive");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    private void downloadAndInstallAPK() {
-        InstallAPK installAPK = new InstallAPK();
-        installAPK.setContext(getActivity(), new ProgressDialog(getActivity()));
-        installAPK.execute(URL_LF_MEETING_ROOM_APP);
-    }
-
-    private void installFromGooglePlay() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("market://details?id=com.zeptolab.thieves.google"));
-        try{
-            startActivity(intent);
-        }
-        catch(Exception e){
-            intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.zeptolab.thieves.google&rdid=com.zeptolab.thieves.google"));
-            startActivity(intent);
-        }
-    }
-
-    private void installSilently(final File file) throws IOException {
-        PackageInstaller packageInstaller = getActivity().getPackageManager().getPackageInstaller();
-        PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL);
-        int sessionID = packageInstaller.createSession(params);
-
-        PackageInstaller.Session session = packageInstaller.openSession(sessionID);
-
-        long sizeBytes = 0;
-        if (file.isFile())
-        {
-            sizeBytes = file.length();
-        }
-
-        InputStream in = null;
-        OutputStream out = null;
-
-        in = new FileInputStream(file);
-        out = session.openWrite("my_app_session", 0, sizeBytes);
-
-        int total = 0;
-        byte[] buffer = new byte[65536];
-        int c;
-        while ((c = in.read(buffer)) != -1)
-        {
-            total += c;
-            out.write(buffer, 0, c);
-        }
-        session.fsync(out);
-        in.close();
-        out.close();
-
-        session.commit(createIntentSender(getActivity(), sessionID));
-
-        session.close();
-    }
-
-
-    // TODO Replace with custom intent
-    private static IntentSender createIntentSender(Context context, int sessionId) {
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                context,
-                sessionId,
-                new Intent(Intent.ACTION_INSTALL_PACKAGE),
-                0);
-        return pendingIntent.getIntentSender();
-    }
-
-    DownloadManager downloadManager;
-    long downloadId;
-
-    private void download() {
-        downloadManager = (DownloadManager)getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-
-        EnterpriseApplicationManager enterpriseApplicationManager = EnterpriseApplicationManager.getInstance();
-
-        for(Application app: enterpriseApplicationManager.getNotInstalledApps()) {
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(app.getDownloadURL()));
-//        request.setDestinationInExternalFilesDir(getActivity(),Environment.DIRECTORY_DOWNLOADS, "app.apk");
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "app.apk");
-            request.setTitle("Downloading "+app.getApplicationName()+"...");
-
-            downloadId = downloadManager.enqueue(request);
-        }
-    }
-
-    BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-                long downloadId = intent.getLongExtra(
-                        DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-                DownloadManager.Query query = new DownloadManager.Query();
-                query.setFilterById(downloadId);
-                Cursor c = downloadManager.query(query);
-                if (c.moveToFirst()) {
-                    int columnIndex = c
-                            .getColumnIndex(DownloadManager.COLUMN_STATUS);
-                    String uriString = c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-                    Uri uri = Uri.parse(uriString);
-                    String path = uri.getPath();
-                    if (DownloadManager.STATUS_SUCCESSFUL == c
-                            .getInt(columnIndex)) {
-                        Timber.d(path);
-                        try {
-                            installSilently(new File(path));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        Timber.d("Download Successful");
-                    }
-                }
-            }
-        }
-    };
 
 }
